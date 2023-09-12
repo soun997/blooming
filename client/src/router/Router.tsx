ea5cf7da064d0ecb5c0c90047dab3d78b249dc5f
kimssumin <ksumin0730@naver.com>
2023-09-12 11:17:19 +0900

    [S09P22A105-343] feat: 페이지 라우팅 설정

--- client/src/router/Router.tsx
+++ client/src/router/Router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from '../pages/MainPage';
+import ArtistPage from '../pages/ListPage/ArtistPage';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />}></Route>
+        <Route path="/artist" element={<ArtistPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
