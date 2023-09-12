fa55bbf40fa14a0e052ebd034eb4690e4c2f1656
rt3310 <btac3310@gmail.com>
2023-09-12 14:02:01 +0900

    initialize project

---
+++ server/Blooming/src/test/java/com/fivengers/blooming/BloomingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fivengers.blooming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloomingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
