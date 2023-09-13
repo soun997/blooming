6103f186b70d7fffa5d883d4870a4a4d46bc8978
kimssumin <ksumin0730@naver.com>
2023-09-13 17:03:12 +0900

    [S09P22A105-349] feat: 펀딩 등록 페이지 필요 정보 타입 선언

--- client/src/types/ProcessInfo.ts
+++ client/src/types/ProcessInfo.ts
@@ -7,3 +7,58 @@ export interface ProcessInfo {
   totalProcess: number;
   nowProcess: number;
 }
+
+export interface FundAddInfo {
+  projectInfo: ProjectInfoInAdd;
+  defaultInfo: DefaultInfoInAdd;
+  storyInfo: StoryInfoInAdd;
+  policyInfo: PolicyInAdd;
+}
+
+export type ProjectInfoInAdd = {
+  category: string;
+  makerInfo: MakerInfo;
+  targetAmount: number;
+};
+
+export type MakerInfo = {
+  makerNum: string;
+  makerName: string;
+  makerAddFile: string;
+  sealCertificate: string;
+};
+
+export type DefaultInfoInAdd = {
+  image: string;
+  startDate: string;
+  endDate: string;
+  title: string;
+};
+
+export type StoryInfoInAdd = {
+  image: string;
+  summary: string;
+  teaser: string;
+  moreInfo: MoreInfo;
+  budget: number;
+};
+
+export type MoreInfo = {
+  album_desc: string;
+  track_list: string;
+  album_img: string;
+};
+
+export type PolicyInAdd = {
+  service: boolean;
+  refund: boolean;
+};
+
+export type RepresentInfo = {
+  refresentor: string;
+  calculateInfo: {
+    email: string;
+    deposit: string;
+    bankbookImage: string;
+  };
+};
