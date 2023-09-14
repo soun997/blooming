9167514f24fb0e2cfc67368f4db3bcdab1251214
rt3310 <btac3310@gmail.com>
2023-09-14 17:26:21 +0900

    [S09P22A105-356] test: NftSaleService 테스트 코드 작성

---
+++ server/Blooming/src/test/java/com/fivengers/blooming/artist/application/NftSaleServiceTest.java
@@ -0,0 +1,56 @@
+package com.fivengers.blooming.artist.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.fivengers.blooming.artist.domain.Artist;
+import com.fivengers.blooming.artist.domain.NftSale;
+import com.fivengers.blooming.fixture.artist.adapter.out.persistence.FakeNftSalePersistenceAdapter;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class NftSaleServiceTest {
+
+    NftSaleService nftSaleService;
+    FakeNftSalePersistenceAdapter nftSalePersistenceAdapter;
+    Artist artist;
+
+    @BeforeEach
+    void initObjects() {
+        LocalDateTime now = LocalDateTime.now();
+        this.artist = Artist.builder()
+                .id(1L)
+                .stageName("아이유")
+                .agency("EDAM 엔터테인먼트")
+                .description("아이유입니다.")
+                .createdAt(now)
+                .modifiedAt(now)
+                .build();
+        this.nftSalePersistenceAdapter = new FakeNftSalePersistenceAdapter();
+        nftSaleService = new NftSaleService(this.nftSalePersistenceAdapter);
+    }
+
+    @Test
+    @DisplayName("아티스트 ID를 통해 nft 집계를 조회한다.")
+    void findNftSaleByArtistId() {
+        // given
+        LocalDateTime now = LocalDateTime.now();
+        NftSale nftSale = NftSale.builder()
+                .id(1L)
+                .issueCount(1)
+                .saleCount(1)
+                .issuesAmount(10000L)
+                .salesAmount(10000L)
+                .createdAt(now)
+                .modifiedAt(now)
+                .deleted(false)
+                .artist(this.artist)
+                .build();
+        nftSalePersistenceAdapter.save(nftSale);
+
+        NftSale searchedNftSale = nftSaleService.searchByArtistId(artist.getId());
+
+        assertThat(searchedNftSale).isEqualTo(nftSale);
+    }
+}
---
+++ server/Blooming/src/test/java/com/fivengers/blooming/fixture/artist/adapter/out/persistence/FakeNftSalePersistenceAdapter.java
@@ -0,0 +1,52 @@
+package com.fivengers.blooming.fixture.artist.adapter.out.persistence;
+
+import com.fivengers.blooming.artist.application.port.out.NftSalePort;
+import com.fivengers.blooming.artist.domain.NftSale;
+import com.fivengers.blooming.global.exception.nftsale.NftSaleNotFoundException;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FakeNftSalePersistenceAdapter implements NftSalePort {
+
+    private final Map<Long, NftSale> store = new HashMap<>();
+    private Long autoIncrementId = 1L;
+
+    public NftSale save(NftSale nftSale) {
+        if (isPersistenceObject(nftSale)) {
+            store.put(nftSale.getId(), nftSale);
+            return nftSale;
+        }
+        return persist(nftSale);
+    }
+
+    @Override
+    public NftSale findByArtistId(Long artistId) {
+        return store.values().stream()
+                .filter(nftSale -> nftSale.getArtist().getId().equals(artistId))
+                .findFirst()
+                .orElseThrow(NftSaleNotFoundException::new);
+    }
+
+    private static boolean isPersistenceObject(NftSale nftSale) {
+        return nftSale.getId() != null;
+    }
+
+    private NftSale persist(NftSale nftSale) {
+        LocalDateTime now = LocalDateTime.now();
+        NftSale persistedNftSale = NftSale.builder()
+                .id(autoIncrementId)
+                .saleCount(nftSale.getSaleCount())
+                .issueCount(nftSale.getIssueCount())
+                .salesAmount(nftSale.getSalesAmount())
+                .issuesAmount(nftSale.getIssuesAmount())
+                .createdAt(now)
+                .modifiedAt(now)
+                .deleted(nftSale.getDeleted())
+                .artist(nftSale.getArtist())
+                .build();
+        store.put(autoIncrementId, persistedNftSale);
+        autoIncrementId++;
+        return persistedNftSale;
+    }
+}
