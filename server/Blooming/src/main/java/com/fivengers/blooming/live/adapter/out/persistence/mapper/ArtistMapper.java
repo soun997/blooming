5debb2933d1ed001389c8db74124c4d2d82af0f6
김주현 <skydreamer21@naver.com>
2023-09-17 17:58:33 +0900

    [S09P22A105-358] 라이브 관련 및 필요 Mapper 구현

---
+++ server/Blooming/src/main/java/com/fivengers/blooming/live/adapter/out/persistence/mapper/ArtistMapper.java
@@ -0,0 +1,27 @@
+package com.fivengers.blooming.live.adapter.out.persistence.mapper;
+
+// TODO : merge 전 임시 클래스 --> 이후 삭제 예정
+
+import com.fivengers.blooming.live.adapter.out.persistence.entity.ArtistJpaEntity;
+import com.fivengers.blooming.live.domain.Artist;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ArtistMapper {
+
+    private final MemberMapper memberMapper;
+
+    public Artist toDomain(ArtistJpaEntity artistJpaEntity) {
+        return new Artist(artistJpaEntity.getId(),
+                artistJpaEntity.getStageName(),
+                memberMapper.toDomain(artistJpaEntity.getMemberJpaEntity()));
+    }
+
+    public ArtistJpaEntity toEntity(Artist artist) {
+        return new ArtistJpaEntity(artist.getId(),
+                artist.getStageName(),
+                memberMapper.toEntity(artist.getMember()));
+    }
+}
