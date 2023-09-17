45564a61dd9639f8c6098c6478bd4f9033159ffe
김주현 <skydreamer21@naver.com>
2023-09-17 17:50:37 +0900

    [S09P22A105-358] feat: 라이브 키워드 검색 application 파트 구현

---
+++ server/Blooming/src/main/java/com/fivengers/blooming/live/application/port/LiveService.java
@@ -0,0 +1,22 @@
+package com.fivengers.blooming.live.application.port;
+
+import com.fivengers.blooming.live.application.port.in.LiveSearchUseCase;
+import com.fivengers.blooming.live.application.port.out.LivePort;
+import com.fivengers.blooming.live.domain.Live;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LiveService implements LiveSearchUseCase {
+
+    private final LivePort livePort;
+
+    @Override
+    public List<Live> searchByKeyword(String query, Pageable pageable) {
+        return livePort.findByKeyword(query, pageable);
+    }
+
+}
