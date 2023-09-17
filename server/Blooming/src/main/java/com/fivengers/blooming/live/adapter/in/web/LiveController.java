12de55cc1afada4e08f05146f4206676f5df32f9
김주현 <skydreamer21@naver.com>
2023-09-17 17:48:47 +0900

    [S09P22A105-358] fix: 라이브 키워드 검색 web 파트 Pageable 사용하도록 수정

--- server/Blooming/src/main/java/com/fivengers/blooming/live/adapter/in/web/LiveController.java
+++ server/Blooming/src/main/java/com/fivengers/blooming/live/adapter/in/web/LiveController.java
@@ -6,9 +6,12 @@ import com.fivengers.blooming.live.adapter.in.web.dto.SearchRequest;
 import com.fivengers.blooming.live.application.port.in.LiveSearchUseCase;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,9 +22,9 @@ public class LiveController {
     private final LiveSearchUseCase liveSearchUseCase;
 
     @GetMapping("/search/keyword")
-    public ApiResponse<List<LiveListResponse>> LiveListByKeyword(SearchRequest searchRequest) {
+    public ApiResponse<List<LiveListResponse>> LiveListByKeyword(@RequestParam String query, Pageable pageable) {
         // TODO : searchRequest Validation
-        List<LiveListResponse> liveResponseList = liveSearchUseCase.searchByKeyword(searchRequest)
+        List<LiveListResponse> liveResponseList = liveSearchUseCase.searchByKeyword(query, pageable)
                 .stream().map(LiveListResponse::from)
                 .toList();
         return new ApiResponse<>(HttpStatus.OK.value(), liveResponseList);
--- server/Blooming/src/main/java/com/fivengers/blooming/live/application/port/in/LiveSearchUseCase.java
+++ server/Blooming/src/main/java/com/fivengers/blooming/live/application/port/in/LiveSearchUseCase.java
@@ -3,8 +3,9 @@ package com.fivengers.blooming.live.application.port.in;
 import com.fivengers.blooming.live.adapter.in.web.dto.SearchRequest;
 import com.fivengers.blooming.live.domain.Live;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 public interface LiveSearchUseCase {
 
-    List<Live> searchByKeyword(SearchRequest searchRequest);
+    List<Live> searchByKeyword(String query, Pageable pageable);
 }
