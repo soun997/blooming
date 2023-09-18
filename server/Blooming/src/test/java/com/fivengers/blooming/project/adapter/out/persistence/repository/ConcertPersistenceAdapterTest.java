6990a5b15409fbc7089bcde13e59b26c37455b53
soyun <soun997@gmail.com>
2023-09-18 18:11:25 +0900

    [S09P22A105-235] test: ConcertPersistenceAdapterTest 구현

---
+++ server/Blooming/src/test/java/com/fivengers/blooming/project/adapter/out/persistence/repository/ConcertPersistenceAdapterTest.java
@@ -0,0 +1,68 @@
+package com.fivengers.blooming.project.adapter.out.persistence.repository;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.fivengers.blooming.artist.adapter.out.persistence.entity.ArtistJpaEntity;
+import com.fivengers.blooming.artist.adapter.out.persistence.mapper.ArtistMapper;
+import com.fivengers.blooming.artist.adapter.out.persistence.repository.ArtistSpringDataRepository;
+import com.fivengers.blooming.project.adapter.out.persistence.entity.ConcertJpaEntity;
+import com.fivengers.blooming.project.adapter.out.persistence.entity.ProjectJpaEntity;
+import com.fivengers.blooming.project.adapter.out.persistence.mapper.ConcertMapper;
+import com.fivengers.blooming.project.domain.Concert;
+import com.fivengers.blooming.project.domain.Project;
+import java.time.LocalDateTime;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import({ConcertPersistenceAdapter.class, ConcertMapper.class, ArtistMapper.class})
+public class ConcertPersistenceAdapterTest {
+
+    @Autowired
+    private ConcertPersistenceAdapter concertPersistenceAdapter;
+    @Autowired
+    private ConcertMapper concertMapper;
+    @Autowired
+    private ConcertSpringDataRepository concertSpringDataRepository;
+    @Autowired
+    private ArtistSpringDataRepository artistSpringDataRepository;
+
+    @Test
+    @DisplayName("전체 콘서트 펀딩 프로젝트 목록을 조회한다.")
+    void findAllConcerts() {
+        // given
+        LocalDateTime now = LocalDateTime.now();
+        ArtistJpaEntity artist = new ArtistJpaEntity();
+        artistSpringDataRepository.save(artist);
+        ArtistJpaEntity artist2 = new ArtistJpaEntity();
+        artistSpringDataRepository.save(artist2);
+
+        ConcertJpaEntity concert1 = ConcertJpaEntity.builder()
+                .name("아이유 데뷔 20주년 콘서트")
+                .startedAt(now)
+                .endedAt(now.plusMonths(3L))
+                .description("아이유 데뷔 20주년을 기념하는 콘서트")
+                .artist(artist)
+                .build();
+        concertSpringDataRepository.save(concert1);
+        ConcertJpaEntity concert2 = ConcertJpaEntity.builder()
+                .name("아이유 데뷔 21주년 콘서트")
+                .startedAt(now)
+                .endedAt(now.plusMonths(3L))
+                .description("아이유 데뷔 20주년을 기념하는 콘서트")
+                .artist(artist)
+                .build();
+        concertSpringDataRepository.save(concert2);
+        List<Concert> concerts =  concertPersistenceAdapter.findAll();
+        assertThat(concerts).containsAll(concerts);
+    }
+}
