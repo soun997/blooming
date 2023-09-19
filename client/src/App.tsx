fb27db6f043cd4b6047769a618f58286589112f2
kimssumin <ksumin0730@naver.com>
2023-09-19 15:27:50 +0900

    [S09P22A105-436] feat: react-query 기본 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css';
 import Router from './router/Router';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Router></Router>
-    </>
+    </QueryClientProvider>
   );
 }
 
