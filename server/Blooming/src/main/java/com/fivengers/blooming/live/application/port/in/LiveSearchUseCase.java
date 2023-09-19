7e21c0acf110ae77b0ec751875df0c368e76af0f
김주현 <skydreamer21@naver.com>
2023-09-19 16:33:42 +0900

    [S09P22A105-358] refactor: 필요없는 dto 삭제

--- server/Blooming/src/main/java/com/fivengers/blooming/live/application/port/in/LiveSearchUseCase.java
+++ server/Blooming/src/main/java/com/fivengers/blooming/live/application/port/in/LiveSearchUseCase.java
@@ -1,6 +1,5 @@
 package com.fivengers.blooming.live.application.port.in;
 
-import com.fivengers.blooming.live.adapter.in.web.dto.SearchRequest;
 import com.fivengers.blooming.live.domain.Live;
 import java.util.List;
 import org.springframework.data.domain.Pageable;
