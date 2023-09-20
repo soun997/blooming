ef9dc541a04a6508fcef554e02e80160227b979c
soyun <soun997@gmail.com>
2023-09-20 13:38:43 +0900

    [S09P22A105-235] test: 콘서트 전체 목록 조회 테스트 - 페이지네이션 검증

--- server/Blooming/src/test/java/com/fivengers/blooming/fixture/project/adapter/out/persistence/FakeConcertPersistenceAdapter.java
+++ server/Blooming/src/test/java/com/fivengers/blooming/fixture/project/adapter/out/persistence/FakeConcertPersistenceAdapter.java
@@ -2,11 +2,13 @@ package com.fivengers.blooming.fixture.project.adapter.out.persistence;
 
 import com.fivengers.blooming.project.application.port.out.ConcertPort;
 import com.fivengers.blooming.project.domain.Concert;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 public class FakeConcertPersistenceAdapter implements ConcertPort {
 
@@ -44,7 +46,34 @@ public class FakeConcertPersistenceAdapter implements ConcertPort {
     }
 
     @Override
-    public List<Concert> findAll() {
-        return store.values().stream().toList();
+    public List<Concert> findAll(Pageable pageable) {
+        if (pageable.getSort().equals(Sort.by("fundingAmount").descending())) {
+            //System.out.println("fundingAmount");
+            return store.values().stream()
+                    .sorted(Comparator.comparing(Concert::getFundingAmount).reversed())
+                    .toList();
+        } else {
+            //System.out.println("createdAt");
+            return store.values().stream()
+                    .sorted(Comparator.comparing(Concert::getCreatedAt))
+                    .toList();
+        }
+    }
+
+    @Override
+    public List<Concert> findAllOngoingProject(Pageable pageable) {
+        if (pageable.getSort().equals(Sort.by("fundingAmount").descending())) {
+            //System.out.println("fundingAmount");
+            return store.values().stream()
+                    .filter(concert -> concert.getEndedAt().isAfter(LocalDateTime.now()))
+                    .sorted(Comparator.comparing(Concert::getFundingAmount).reversed())
+                    .toList();
+        } else {
+            //System.out.println("createdAt");
+            return store.values().stream()
+                    .filter(concert -> concert.getEndedAt().isAfter(LocalDateTime.now()))
+                    .sorted(Comparator.comparing(Concert::getCreatedAt))
+                    .toList();
+        }
     }
 }
