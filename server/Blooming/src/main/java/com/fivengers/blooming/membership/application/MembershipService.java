d35749613f88acd861caf10a47f901fc7ee222ae
rt3310 <btac3310@gmail.com>
2023-09-20 16:34:46 +0900

    [S09P22A105-369] feature: Membership 등록 기능 비즈니스 로직 구현

--- server/Blooming/src/main/java/com/fivengers/blooming/membership/application/MembershipService.java
+++ server/Blooming/src/main/java/com/fivengers/blooming/membership/application/MembershipService.java
@@ -1,8 +1,11 @@
 package com.fivengers.blooming.membership.application;
 
+import com.fivengers.blooming.artist.application.port.out.ArtistPort;
 import com.fivengers.blooming.membership.application.port.in.MembershipUseCase;
+import com.fivengers.blooming.membership.application.port.in.dto.MembershipCreateRequest;
 import com.fivengers.blooming.membership.application.port.out.MembershipPort;
 import com.fivengers.blooming.membership.domain.Membership;
+import com.fivengers.blooming.nft.port.out.NftPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,6 +16,15 @@ import org.springframework.stereotype.Service;
 public class MembershipService implements MembershipUseCase {
 
     private final MembershipPort membershipPort;
+    private final ArtistPort artistPort;
+    private final NftPort nftPort;
+
+    @Override
+    public Membership add(MembershipCreateRequest request) {
+        return membershipPort.save(request.toDomain(
+                artistPort.findById(request.artistId()),
+                nftPort.findById(request.nftId())));
+    }
 
     @Override
     public Page<Membership> searchLatestSeasons(Pageable pageable) {
