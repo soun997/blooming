ac61ba0ceaa641ba77d84364a778cb176b68ac30
kimssumin <ksumin0730@naver.com>
2023-09-21 08:53:49 +0900

    [S09P22A105-openvidu] fix: routing error

--- client/src/router/Router.tsx
+++ client/src/router/Router.tsx
@@ -6,7 +6,6 @@ import NFTList from '@pages/ListPage/NFTList';
 import ConcertList from '@pages/ListPage/ConcertList';
 import ActiveList from '@pages/ListPage/ActiveList';
 import AddFund from '@pages/AddFund';
-import ActiveDetail from '@pages/DetailPage/ActiveDetail';
 import ArtistDetail from '@pages/DetailPage/ArtistDetail';
 import MeetingPage from '@pages/MeetingPage/MeetingPage';
 
@@ -15,12 +14,11 @@ export default function Router() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />}></Route>
-        <Route path="/activedetail" element={<ActiveDetail />}></Route>
         <Route path="/nft" element={<NFTList />}></Route>
         <Route path="/concert" element={<ConcertList />}></Route>
         <Route path="/active" element={<ActiveList />}></Route>
         <Route path="/add-fund" element={<AddFund />}></Route>
-         <Route path="/active-detail" element={<ActiveDetail />}></Route>
+        <Route path="/active-detail" element={<ActiveDetail />}></Route>
         <Route path="/artist-detail" element={<ArtistDetail />}></Route>
         <Route
           path="/meeting-artist"
