4d4674e6b9340d354ba64d650fcaba9f66b68547
rt3310 <btac3310@gmail.com>
2023-09-22 01:05:59 +0900

    [S09P22A105-357] fix: ArtistScrapController 메서드 ResponseStatus 를 no content(204)로 변경

--- server/Blooming/src/main/java/com/fivengers/blooming/artistscrap/adapter/in/web/ArtistScrapController.java
+++ server/Blooming/src/main/java/com/fivengers/blooming/artistscrap/adapter/in/web/ArtistScrapController.java
@@ -4,11 +4,13 @@ import com.fivengers.blooming.artistscrap.application.port.in.ArtistScrapUseCase
 import com.fivengers.blooming.artistscrap.application.port.in.dto.ArtistScrapRequest;
 import com.fivengers.blooming.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,6 +21,7 @@ public class ArtistScrapController {
     private final ArtistScrapUseCase artistScrapUseCase;
 
     @PostMapping("/scrap")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public ApiResponse<Object> scrapCreate(@PathVariable Long artistId,
                                            @RequestBody @Validated ArtistScrapRequest request) {
         artistScrapUseCase.scrap(request, artistId);
@@ -26,6 +29,7 @@ public class ArtistScrapController {
     }
 
     @PostMapping("/unscrap")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public ApiResponse<Object> scrapRemove(@PathVariable Long artistId,
                                            @RequestBody @Validated ArtistScrapRequest request) {
         artistScrapUseCase.unScrap(request, artistId);
