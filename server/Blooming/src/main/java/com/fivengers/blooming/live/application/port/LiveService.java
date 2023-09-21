46852e61937188dfb09b9952359b2ec63da8a201
김주현 <skydreamer21@naver.com>
2023-09-21 20:14:25 +0900

    [S09P22A105-450] fix: Page 정보 추가 관련 application 파트 수정

--- server/Blooming/src/main/java/com/fivengers/blooming/live/application/port/LiveService.java
+++ server/Blooming/src/main/java/com/fivengers/blooming/live/application/port/LiveService.java
@@ -5,6 +5,7 @@ import com.fivengers.blooming.live.application.port.out.LivePort;
 import com.fivengers.blooming.live.domain.Live;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -15,12 +16,12 @@ public class LiveService implements LiveSearchUseCase {
     private final LivePort livePort;
 
     @Override
-    public List<Live> searchByKeyword(String query, Pageable pageable) {
+    public Page<Live> searchByKeyword(String query, Pageable pageable) {
         return livePort.findByKeyword(query, pageable);
     }
 
     @Override
-    public List<Live> searchByArtist(String query, Pageable pageable) {
+    public Page<Live> searchByArtist(String query, Pageable pageable) {
         return livePort.findByArtistStageName(query, pageable);
     }
 
--- server/Blooming/src/main/java/com/fivengers/blooming/live/application/port/in/LiveSearchUseCase.java
+++ server/Blooming/src/main/java/com/fivengers/blooming/live/application/port/in/LiveSearchUseCase.java
@@ -2,11 +2,12 @@ package com.fivengers.blooming.live.application.port.in;
 
 import com.fivengers.blooming.live.domain.Live;
 import java.util.List;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface LiveSearchUseCase {
 
-    List<Live> searchByKeyword(String query, Pageable pageable);
+    Page<Live> searchByKeyword(String query, Pageable pageable);
 
-    List<Live> searchByArtist(String query, Pageable pageable);
+    Page<Live> searchByArtist(String query, Pageable pageable);
 }
