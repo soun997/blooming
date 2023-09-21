336f6c106781830cb53f409376d92160ec9c0a61
soyun <soun997@gmail.com>
2023-09-21 14:48:09 +0900

    [S09P22A105-236] feature: custom 예외 추가

--- server/Blooming/src/main/java/com/fivengers/blooming/global/exception/ExceptionCode.java
+++ server/Blooming/src/main/java/com/fivengers/blooming/global/exception/ExceptionCode.java
@@ -11,7 +11,11 @@ public enum ExceptionCode {
 
     NFT_NOT_FOUND(HttpStatus.NOT_FOUND, "ERR_NFT_001", "NFT를 찾을 수 없습니다"),
 
-    ARTIST_NOT_FOUND(HttpStatus.NOT_FOUND, "ERR_ARTIST_001", "아티스트를 찾을 수 없습니다.");
+    ARTIST_NOT_FOUND(HttpStatus.NOT_FOUND, "ERR_ARTIST_001", "아티스트를 찾을 수 없습니다."),
+
+    PROJECT_NOT_FOUND(HttpStatus.NOT_FOUND, "ERR_PROJECT_001", "펀딩 프로젝트를 찾을 수 없습니다."),
+
+    INVESTMENT_OVERVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "ERR_INVESTMENT_OVERVIEW_001", "투자 개요를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String errorCode;
