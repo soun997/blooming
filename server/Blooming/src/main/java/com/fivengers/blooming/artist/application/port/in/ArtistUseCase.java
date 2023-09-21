cd68dbfd892c5b7ee34224363982047756a2ad4c
rt3310 <btac3310@gmail.com>
2023-09-21 15:19:45 +0900

    [S09P22A105-368] feature: 아티스트에 등록 기능 비즈니스 로직 구현

--- server/Blooming/src/main/java/com/fivengers/blooming/artist/application/port/in/ArtistUseCase.java
+++ server/Blooming/src/main/java/com/fivengers/blooming/artist/application/port/in/ArtistUseCase.java
@@ -1,10 +1,12 @@
 package com.fivengers.blooming.artist.application.port.in;
 
+import com.fivengers.blooming.artist.application.port.in.dto.ArtistCreateRequest;
 import com.fivengers.blooming.artist.domain.Artist;
 import java.util.List;
 
 public interface ArtistUseCase {
 
+    Artist add(ArtistCreateRequest request, Long memberId);
     List<Artist> searchAll();
     Artist searchById(Long artistId);
 
