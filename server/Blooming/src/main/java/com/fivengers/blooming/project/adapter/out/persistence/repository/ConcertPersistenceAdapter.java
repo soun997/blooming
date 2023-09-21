e7ba56264f6c69a3477778e80f4076bec04fafa6
soyun <soun997@gmail.com>
2023-09-21 14:47:42 +0900

    [S09P22A105-236] feature: 콘서트 상세 조회 persistence 파트 구현

--- server/Blooming/src/main/java/com/fivengers/blooming/project/adapter/out/persistence/repository/ConcertPersistenceAdapter.java
+++ server/Blooming/src/main/java/com/fivengers/blooming/project/adapter/out/persistence/repository/ConcertPersistenceAdapter.java
@@ -1,10 +1,13 @@
 package com.fivengers.blooming.project.adapter.out.persistence.repository;
 
 
+import com.fivengers.blooming.artist.domain.Artist;
+import com.fivengers.blooming.global.exception.project.ProjectNotFoundException;
 import com.fivengers.blooming.project.adapter.out.persistence.entity.ConcertJpaEntity;
 import com.fivengers.blooming.project.adapter.out.persistence.mapper.ConcertMapper;
 import com.fivengers.blooming.project.application.port.out.ConcertPort;
 import com.fivengers.blooming.project.domain.Concert;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -34,5 +37,27 @@ public class ConcertPersistenceAdapter implements ConcertPort {
         return new PageImpl<>(concerts.stream()
                 .map(concertMapper::toDomain)
                 .toList(), pageable, concerts.getTotalElements());
+    }
+
+    @Override
+    public Page<Concert> findAllByArtist(Artist artist, Pageable pageable) {
+        Page<ConcertJpaEntity> concerts = concertSpringDataRepository.findAllByArtist(artist, pageable);
+        return new PageImpl<>(concerts.stream()
+                .map(concertMapper::toDomain)
+                .toList(), pageable, concerts.getTotalElements());
+    }
+
+    @Override
+    public List<Concert> findAllPastConcertByArtist(Artist artist, Pageable pageable) {
+        return concertSpringDataRepository.findAllFinishedProjectByArtist(artist, pageable).stream()
+                .map(concertMapper::toDomain)
+                .toList();
+    }
+
+    @Override
+    public Concert findById(Long id) {
+        return concertSpringDataRepository.findById(id)
+                .map(concertMapper::toDomain)
+                .orElseThrow(ProjectNotFoundException::new);
     }
 }
