8117640c0551ad19d5e015d154208b737d9f08c1
rt3310 <btac3310@gmail.com>
2023-09-22 07:46:51 +0900

    [S09P22A105-357] feature: Artist 스크랩 기록 Port 인터페이스 정의

--- server/Blooming/src/main/java/com/fivengers/blooming/artistscrap/application/port/out/ArtistScrapRecordPort.java
+++ server/Blooming/src/main/java/com/fivengers/blooming/artistscrap/application/port/out/ArtistScrapRecordPort.java
@@ -1,5 +1,14 @@
 package com.fivengers.blooming.artistscrap.application.port.out;
 
+import com.fivengers.blooming.artistscrap.domain.ArtistScrapRecord;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 public interface ArtistScrapRecordPort {
 
+    ArtistScrapRecord save(ArtistScrapRecord artistScrapRecord);
+    Optional<ArtistScrapRecord> findByStartDateAndEndData(LocalDateTime startDate,
+                                                          LocalDateTime endDate);
+
+    void update(ArtistScrapRecord artistScrapRecord);
 }
