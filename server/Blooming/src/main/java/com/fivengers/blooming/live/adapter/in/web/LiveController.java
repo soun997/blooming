6cff3bc29e513166165d4f76b6f1ff96ab28c77b
김주현 <skydreamer21@naver.com>
2023-09-21 21:00:04 +0900

    [S09P22A105-450] fix: Live web api Url 수정

--- server/Blooming/src/main/java/com/fivengers/blooming/live/adapter/in/web/LiveController.java
+++ server/Blooming/src/main/java/com/fivengers/blooming/live/adapter/in/web/LiveController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Validated
 @RestController
-@RequestMapping("/api/v1/live")
+@RequestMapping("/api/v1/lives")
 @RequiredArgsConstructor
 public class LiveController {
 
