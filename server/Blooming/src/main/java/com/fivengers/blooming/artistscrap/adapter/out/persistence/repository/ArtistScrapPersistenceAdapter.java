a7c43db6f007c9f91f5d646c1550d6bc9be20101
rt3310 <btac3310@gmail.com>
2023-09-22 10:43:49 +0900

    [S09P22A105-357] [S09P22A105-357] feature: Artist 관심 기능 persistence 로직 구현

--- server/Blooming/src/main/java/com/fivengers/blooming/artistscrap/adapter/out/persistence/repository/ArtistScrapPersistenceAdapter.java
+++ server/Blooming/src/main/java/com/fivengers/blooming/artistscrap/adapter/out/persistence/repository/ArtistScrapPersistenceAdapter.java
@@ -3,6 +3,7 @@ package com.fivengers.blooming.artistscrap.adapter.out.persistence.repository;
 import com.fivengers.blooming.artistscrap.adapter.out.persistence.mapper.ArtistScrapMapper;
 import com.fivengers.blooming.artistscrap.application.port.out.ArtistScrapPort;
 import com.fivengers.blooming.artistscrap.domain.ArtistScrap;
+import com.fivengers.blooming.global.exception.artistscrap.ArtistScrapNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,6 +24,14 @@ public class ArtistScrapPersistenceAdapter implements ArtistScrapPort {
     @Override
     @Transactional
     public void deleteScrap(Long memberId, Long artistId) {
-        artistScrapSpringDataRepository.deleteByMemberJpaEntityIdAndArtistJpaEntityId(memberId, artistId);
+        artistScrapSpringDataRepository
+                .deleteByMemberJpaEntityIdAndArtistJpaEntityId(memberId, artistId);
+    }
+
+    @Override
+    public ArtistScrap findByMemberIdAndArtistId(Long memberId, Long artistId) {
+        return artistScrapMapper.toDomain(artistScrapSpringDataRepository
+                .findByMemberJpaEntityIdAndArtistJpaEntityId(memberId, artistId)
+                .orElseThrow(ArtistScrapNotFoundException::new));
     }
 }
