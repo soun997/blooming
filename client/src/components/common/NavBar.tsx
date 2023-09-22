ea966b2ebe3375b06ef9018919ad278fe35bcf85
kimssumin <ksumin0730@naver.com>
2023-09-22 17:17:02 +0900

    [S09P22A105-458] feat: nav 바 구현

---
+++ client/src/components/common/NavBar.tsx
@@ -0,0 +1,125 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { ReactComponent as UserSvg } from '@assets/icons/user-key.svg';
+import { ReactComponent as MyPageSvg } from '@assets/icons/account-mypage.svg';
+import { ReactComponent as ModifSvg } from '@assets/icons/keymodify.svg';
+import { useNavigate } from 'react-router';
+
+const Navbar = () => {
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!isDropdownOpen);
+  };
+
+  const closeDropdown = () => {
+    setDropdownOpen(false);
+  };
+
+  return (
+    <Nav>
+      <Logo>LOGO</Logo>
+      <NavList>
+        <NavItem onClick={() => navigate('/nft')}>NFT</NavItem>
+        <NavItem onClick={() => navigate('/concert')}>콘서트 펀딩</NavItem>
+        <NavItem onClick={() => navigate('/active')}>활동 펀딩</NavItem>
+        <NavItem onClick={() => navigate('/live')}>진행 중인 라이브</NavItem>
+      </NavList>
+      <UserIcon onClick={toggleDropdown}>
+        <div className="user">
+          <UserSvg />
+          사용자
+        </div>
+
+        {isDropdownOpen && (
+          <Dropdown onClick={(e) => e.stopPropagation()}>
+            <DropdownItem onClick={() => navigate('/mypage')}>
+              <MyPageSvg />
+              마이페이지
+            </DropdownItem>
+            <DropdownItem>
+              <ModifSvg />
+              로그아웃
+            </DropdownItem>
+          </Dropdown>
+        )}
+      </UserIcon>
+    </Nav>
+  );
+};
+
+const Nav = styled.nav`
+  display: flex;
+  margin: 0 -280px;
+  justify-content: space-between;
+  align-items: center;
+  background-color: var(--white-color);
+  color: black;
+  padding: 10px 40px;
+  height: 40px;
+  box-shadow: 0px 2px 6px rgba(91, 89, 89, 0.1); /* 그림자 추가 */
+`;
+
+const Logo = styled.div`
+  font-size: 24px;
+`;
+
+const NavList = styled.ul`
+  list-style: none;
+  display: flex;
+  gap: 100px;
+`;
+
+const NavItem = styled.li`
+  cursor: pointer;
+  font-size: 18px;
+  &:hover {
+    color: var(--main1-color);
+    font-weight: 500;
+  }
+`;
+
+const UserIcon = styled.div`
+  position: relative;
+  cursor: pointer;
+
+  .user {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+
+    svg {
+      width: 20px;
+    }
+  }
+`;
+
+const Dropdown = styled.div`
+  position: absolute;
+  top: 120%;
+  right: 0;
+  background-color: var(--white-color);
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+  border-radius: 5px;
+  width: 150px;
+  padding: 10px 0px;
+`;
+
+const DropdownItem = styled.div`
+  cursor: pointer;
+  display: flex;
+  padding: 10px 20px;
+  justify-content: space-between;
+  align-items: center;
+
+  svg {
+    width: 15px;
+  }
+  &:hover {
+    color: var(--main1-color);
+    cursor: pointer;
+  }
+`;
+
+export default Navbar;
