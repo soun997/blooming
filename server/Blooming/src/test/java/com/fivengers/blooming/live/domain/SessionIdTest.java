22fdb6126d1262b37949e6be6ca7334c9ed50bc8
김주현 <skydreamer21@naver.com>
2023-09-23 20:44:40 +0900

    [S09P22A105-360] test: sessionId에서 LiveId를 가져오는 로직 테스트 구현

--- server/Blooming/src/test/java/com/fivengers/blooming/live/domain/SessionIdTest.java
+++ server/Blooming/src/test/java/com/fivengers/blooming/live/domain/SessionIdTest.java
@@ -1,10 +1,15 @@
 package com.fivengers.blooming.live.domain;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.fivengers.blooming.global.exception.live.InvalidSessionIdException;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class SessionIdTest {
@@ -23,5 +28,32 @@ class SessionIdTest {
         assertThatThrownBy(() -> SessionId.validate(sessionId))
                 .isInstanceOf(InvalidSessionIdException.class);
     }
+
+    @DisplayName("SessionId에서 LiveId를 가져올 수 있다.")
+    @ParameterizedTest(name = "{index}) sessionId = {0}, liveId = {1}")
+    @MethodSource("providedSessionIdAndLiveId")
+    public void SessionId에서_LiveId를_가져올_수_있다(String sessionIdInput, Long liveId) {
+        SessionId sessionId = new SessionId(sessionIdInput);
+        assertThat(sessionId.getLiveId()).isEqualTo(liveId);
+    }
+
+    private static Stream<Arguments> providedSessionIdAndLiveId() {
+        return Stream.of(
+                Arguments.of("blooming1", 1L),
+                Arguments.of("blooming2", 2L),
+                Arguments.of("blooming100", 100L)
+
+        );
+    }
+
+    @DisplayName("SessionId에서 LiveId를 가져올 때 유효한 SessionId가 아니라면 InvalidSessionIdException이 발생한다.")
+    @ParameterizedTest(name = "{index}) sessionId = {0}")
+    @ValueSource(strings = {"", " ", "abc", "blooming", "abcblooming", "bloomingabc", "bloomingasd1", "blooming2sf", "blooming3 "})
+    public void SessionId에서_LiveId를_가져올_때_유효한_SessionId가_아니라면_InvalidSessionIdException이_발생한다(String sessionIdInput) {
+        SessionId sessionId = new SessionId(sessionIdInput);
+        assertThatThrownBy(() -> sessionId.getLiveId())
+                .isInstanceOf(InvalidSessionIdException.class);
+    }
+
 
 }
