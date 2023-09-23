5bc5a2bae6b0dbe12282701c253e2843f5a8b557
김주현 <skydreamer21@naver.com>
2023-09-23 19:28:37 +0900

    [S09P22A105-360] fix: 잘못된 변수명 수정

--- server/Blooming/src/main/java/com/fivengers/blooming/live/adapter/in/web/dto/ConnectionTokenDetailRequest.java
+++ server/Blooming/src/main/java/com/fivengers/blooming/live/adapter/in/web/dto/ConnectionTokenDetailRequest.java
@@ -2,6 +2,7 @@ package com.fivengers.blooming.live.adapter.in.web.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record ConnectionTokenDetailRequest(@NotBlank String connectionToken) {
+public record ConnectionTokenDetailRequest(@NotBlank String sessionId) {
+    // TODO : sessionId 규칙 검증 ValidationAnnotation 작성
 
 }
