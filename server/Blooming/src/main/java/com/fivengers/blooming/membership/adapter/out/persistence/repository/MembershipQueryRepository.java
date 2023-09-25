7038fa21cc54cc7b9b47f1c6989ccfe1cdfb87fe
rt3310 <btac3310@gmail.com>
2023-09-25 12:40:36 +0900

    [S09P22A105-357] refactor: findLatestSeasonsGroupByArtist 메서드 분리

--- server/Blooming/src/main/java/com/fivengers/blooming/membership/adapter/out/persistence/repository/MembershipQueryRepository.java
+++ server/Blooming/src/main/java/com/fivengers/blooming/membership/adapter/out/persistence/repository/MembershipQueryRepository.java
@@ -5,6 +5,9 @@ import static com.fivengers.blooming.membership.adapter.out.persistence.entity.Q
 import com.fivengers.blooming.global.support.QuerydslRepositorySupport;
 import com.fivengers.blooming.membership.adapter.out.persistence.entity.MembershipJpaEntity;
 import com.fivengers.blooming.membership.adapter.out.persistence.entity.QMembershipJpaEntity;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.function.Function;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
@@ -20,17 +23,26 @@ public class MembershipQueryRepository extends QuerydslRepositorySupport {
         QMembershipJpaEntity sub = new QMembershipJpaEntity("sub");
 
         return applyPagination(pageable,
-                query -> query.selectFrom(membershipJpaEntity)
-                        .innerJoin(membershipJpaEntity.nftSaleJpaEntity).fetchJoin()
-                        .where(membershipJpaEntity.deleted.eq(false)
-                                .and(membershipJpaEntity.artistJpaEntity.in(
-                                        select(sub.artistJpaEntity)
-                                                .from(sub)
-                                                .groupBy(sub.artistJpaEntity)
-                                                .having(membershipJpaEntity.season.eq(sub.season.max())))))
-                        .offset(pageable.getOffset())
-                        .limit(pageable.getPageSize()),
-                countQuery -> select(membershipJpaEntity.count())
-                        .from(membershipJpaEntity));
+                latestSeasonsContentQuery(pageable, sub),
+                latestSeasonsCountQuery());
+    }
+
+    private Function<JPAQueryFactory, JPAQuery<MembershipJpaEntity>> latestSeasonsContentQuery(
+            Pageable pageable, QMembershipJpaEntity sub) {
+        return query -> query.selectFrom(membershipJpaEntity)
+                .innerJoin(membershipJpaEntity.nftSaleJpaEntity).fetchJoin()
+                .where(membershipJpaEntity.deleted.eq(false)
+                        .and(membershipJpaEntity.artistJpaEntity.in(
+                                select(sub.artistJpaEntity)
+                                        .from(sub)
+                                        .groupBy(sub.artistJpaEntity)
+                                        .having(membershipJpaEntity.season.eq(sub.season.max())))))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize());
+    }
+
+    private Function<JPAQueryFactory, JPAQuery<Long>> latestSeasonsCountQuery() {
+        return countQuery -> select(membershipJpaEntity.count())
+                .from(membershipJpaEntity);
     }
 }
