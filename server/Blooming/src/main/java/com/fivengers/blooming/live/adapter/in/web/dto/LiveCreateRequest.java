a8cf623f9fa2463ae373dcd2609857bc1d7774a0
김주현 <skydreamer21@naver.com>
2023-09-25 13:03:08 +0900

    [S09P22A105-315] fix: LiveCreateRequest 필드 수정

--- server/Blooming/src/main/java/com/fivengers/blooming/live/adapter/in/web/dto/LiveCreateRequest.java
+++ server/Blooming/src/main/java/com/fivengers/blooming/live/adapter/in/web/dto/LiveCreateRequest.java
@@ -1,9 +1,11 @@
 package com.fivengers.blooming.live.adapter.in.web.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 
 public record LiveCreateRequest(
-        @NotBlank String liveTitle
+        @NotBlank String liveTitle,
+        @Min(1) Long artistId
 ) {
 
 }
