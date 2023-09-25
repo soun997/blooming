a88c85cd8a26305a26224839fba6e7ccca786b88
김주현 <skydreamer21@naver.com>
2023-09-25 21:03:05 +0900

    [S09P22A105-315] fix: Live 등록 파라미터에 NotNull 검증 추가

--- server/Blooming/src/main/java/com/fivengers/blooming/live/adapter/in/web/dto/LiveCreateRequest.java
+++ server/Blooming/src/main/java/com/fivengers/blooming/live/adapter/in/web/dto/LiveCreateRequest.java
@@ -2,10 +2,11 @@ package com.fivengers.blooming.live.adapter.in.web.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record LiveCreateRequest(
         @NotBlank String liveTitle,
-        @Min(1) Long artistId
+        @NotNull @Min(1) Long artistId
 ) {
 
 }
