8f8bd9914a1fa053e0744834bc42abbc11bc712e
김주현 <skydreamer21@naver.com>
2023-09-26 15:12:27 +0900

    [S09P22A105-363] feature: 라이브 빈도수 조회 api persistence 파트 구현

--- server/Blooming/src/main/java/com/fivengers/blooming/live/adapter/out/persistence/repository/LiveSpringDataRepository.java
+++ server/Blooming/src/main/java/com/fivengers/blooming/live/adapter/out/persistence/repository/LiveSpringDataRepository.java
@@ -1,9 +1,15 @@
 package com.fivengers.blooming.live.adapter.out.persistence.repository;
 
 import com.fivengers.blooming.live.adapter.out.persistence.entity.LiveJpaEntity;
+import java.time.LocalDateTime;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LiveSpringDataRepository extends JpaRepository<LiveJpaEntity, Long> {
     Optional<LiveJpaEntity> findLiveJpaEntityByIdAndEndedAtIsNull(Long id);
+
+    int countLiveJpaEntitiesByArtistJpaEntity_IdAndCreatedAtBetween(
+            Long artistId,
+            LocalDateTime startDateTime,
+            LocalDateTime endDateTime);
 }
