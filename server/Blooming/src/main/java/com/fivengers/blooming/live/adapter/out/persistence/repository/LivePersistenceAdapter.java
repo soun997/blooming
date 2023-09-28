52d9ecd48190c5214140b045657a73c557e72dd8
김주현 <skydreamer21@naver.com>
2023-09-28 11:16:06 +0900

    [S09P22A105-316] feature: 인기 라이브 조회 api persistence 파트 구현

--- server/Blooming/src/main/java/com/fivengers/blooming/live/adapter/out/persistence/repository/LivePersistenceAdapter.java
+++ server/Blooming/src/main/java/com/fivengers/blooming/live/adapter/out/persistence/repository/LivePersistenceAdapter.java
@@ -1,6 +1,5 @@
 package com.fivengers.blooming.live.adapter.out.persistence.repository;
 
-import com.fivengers.blooming.global.support.QuerydslRepositorySupport;
 import com.fivengers.blooming.live.adapter.out.persistence.entity.LiveJpaEntity;
 import com.fivengers.blooming.live.adapter.out.persistence.mapper.LiveMapper;
 import com.fivengers.blooming.live.application.port.out.LivePort;
@@ -8,25 +7,41 @@ import com.fivengers.blooming.live.domain.Live;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations.TypedTuple;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
+@Slf4j
 @Repository
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class LivePersistenceAdapter implements LivePort {
 
+    private final String KEY = "activeLive";
+
     private final LiveMapper liveMapper;
     private final LiveQueryRepository liveQueryRepository;
     private final LiveSpringDataRepository liveSpringDataRepository;
+    private final LiveRedisRepository liveRedisRepository;
+    private final RedisTemplate<String, Long> redisTemplate;
 
     @Override
     public Page<Live> findByKeyword(String keyword, Pageable pageable) {
-        Page<LiveJpaEntity> liveJpaEntities = liveQueryRepository.findActiveLiveByTitleKeyword(keyword, pageable);
+        Page<LiveJpaEntity> liveJpaEntities = liveQueryRepository.findActiveLiveByTitleKeyword(
+                keyword, pageable);
         return new PageImpl<>(
                 liveJpaEntities.stream().map(liveMapper::toDomain).toList(),
                 pageable,
@@ -57,7 +72,8 @@ public class LivePersistenceAdapter implements LivePort {
 
     @Override
     public Live save(Live live) {
-        LiveJpaEntity createdLiveJpaEntity = liveSpringDataRepository.save(liveMapper.toJpaEntity(live));
+        LiveJpaEntity createdLiveJpaEntity = liveSpringDataRepository.save(
+                liveMapper.toJpaEntity(live));
         return liveMapper.toDomain(createdLiveJpaEntity);
     }
 
@@ -78,5 +94,27 @@ public class LivePersistenceAdapter implements LivePort {
                 pageable,
                 liveJpaEntities.getTotalElements()
         );
+    }
+
+    @Override
+    public List<Live> findTopLivesByNumberOfViewers(int numberOfLives) {
+        Set<TypedTuple<Long>> topActiveLiveTuples = redisTemplate.opsForZSet()
+                .rangeByScoreWithScores(KEY, 0, 1000, 0, numberOfLives);
+        Assert.notNull(topActiveLiveTuples, "시청자 수 정보가 null 일 수 없습니다.");
+
+        Map<Long, Integer> topActiveLivesViewerInfo = convertTupleToMap(topActiveLiveTuples);
+        List<Live> topLives = new ArrayList<>(liveSpringDataRepository.findLiveJpaEntityByIdIsIn(
+                topActiveLivesViewerInfo.keySet()
+        ).stream().map(liveMapper::toDomain).toList());
+        topLives.forEach(live -> live.setNumberOfViewers(topActiveLivesViewerInfo.get(live.getId())));
+        topLives.sort((live1, live2) -> live2.getNumberOfViewers() - live1.getNumberOfViewers());
+        return Collections.unmodifiableList(topLives);
+    }
+
+    private Map<Long, Integer> convertTupleToMap(Set<TypedTuple<Long>> typedTuples) {
+        return typedTuples.stream().collect(Collectors.toMap(
+                tuple -> Long.valueOf(String.valueOf(tuple.getValue())),
+                tuple -> tuple.getScore().intValue()
+        ));
     }
 }
