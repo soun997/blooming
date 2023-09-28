8792fbf9e2850cdcee72ef2ff70b20fff0d0fa89
김주현 <skydreamer21@naver.com>
2023-09-28 16:02:17 +0900

    [S09P22A105-528] fix: 아티스트의 진행중인 라이브 api web 파트 수정

--- server/Blooming/src/main/java/com/fivengers/blooming/live/adapter/in/web/dto/LiveCheckActiveResponse.java
+++ server/Blooming/src/main/java/com/fivengers/blooming/live/adapter/in/web/dto/LiveCheckActiveResponse.java
@@ -1,7 +1,7 @@
 package com.fivengers.blooming.live.adapter.in.web.dto;
 
 public record LiveCheckActiveResponse (
-        Long liveId,
-        boolean isActive) {
+        Long artistId,
+        Long activeLiveId) {
 
 }
