05c8242eab0f47a0b0d5b57cd8d467a398bdfc35
soyun <soun997@gmail.com>
2023-09-30 05:31:03 +0900

    [S09P22A105-362] rename: 패키지 이름 변경 form -> project_application

--- server/Blooming/src/main/java/com/fivengers/blooming/form/application/port/in/FundAddFormUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.fivengers.blooming.form.application.port.in;
-
-import com.fivengers.blooming.form.domain.FundAddForm;
-
-public interface FundAddFormUseCase {
-
-    void addFundAddForm(FundAddForm form);
-}
