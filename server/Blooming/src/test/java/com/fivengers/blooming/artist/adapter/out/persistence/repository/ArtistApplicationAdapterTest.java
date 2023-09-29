bf9d10720096422b404d2b33a0f84fce4286ee23
rt3310 <btac3310@gmail.com>
2023-09-29 17:53:17 +0900

    [S09P22A105-529] test: 아티스트 신청 등록 및 상태 별 조회 기능 adapter 테스트 작성

---
+++ server/Blooming/src/test/java/com/fivengers/blooming/artist/adapter/out/persistence/repository/ArtistApplicationAdapterTest.java
@@ -0,0 +1,130 @@
+package com.fivengers.blooming.artist.adapter.out.persistence.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+import com.fivengers.blooming.artist.domain.ArtistApplication;
+import com.fivengers.blooming.artist.domain.ArtistApplicationState;
+import com.fivengers.blooming.member.adapter.out.persistence.entity.MemberJpaEntity;
+import com.fivengers.blooming.member.adapter.out.persistence.entity.Oauth;
+import com.fivengers.blooming.member.adapter.out.persistence.mapper.MemberMapper;
+import com.fivengers.blooming.member.adapter.out.persistence.repository.MemberSpringDataRepository;
+import com.fivengers.blooming.member.domain.AuthProvider;
+import java.time.LocalDateTime;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+@SpringBootTest
+class ArtistApplicationAdapterTest {
+
+    @Autowired MemberSpringDataRepository memberSpringDataRepository;
+    @Autowired MemberMapper memberMapper;
+    @Autowired ArtistApplicationAdapter artistApplicationAdapter;
+    MemberJpaEntity member1;
+    MemberJpaEntity member2;
+
+    @BeforeEach
+    void initObjects() {
+        member1 = memberSpringDataRepository.save(MemberJpaEntity.builder()
+                .oauth(new Oauth(AuthProvider.KAKAO, "1234567"))
+                .name("이지은")
+                .nickname("아이유")
+                .account("12345678")
+                .deleted(false)
+                .build());
+        member2 = memberSpringDataRepository.save(MemberJpaEntity.builder()
+                .oauth(new Oauth(AuthProvider.KAKAO, "7654321"))
+                .name("박효신")
+                .nickname("박효신")
+                .account("87654321")
+                .deleted(false)
+                .build());
+    }
+
+    @Test
+    @DisplayName("아티스트 신청을 저장한다.")
+    void saveArtistApplication() {
+        LocalDateTime now = LocalDateTime.now();
+        ArtistApplication artistApplication = ArtistApplication.builder()
+                .stageName("아이유 (IU)")
+                .description("아이유입니다.")
+                .agency("EDAM 엔터테인먼트")
+                .applicationState(ArtistApplicationState.APPLY)
+                .profileImageUrl("https://image.com/iu")
+                .youtubeUrl("https://www.youtube.com/iu")
+                .fanCafeUrl("https://cafe.daum.net/iu")
+                .snsUrl("https://instagram.com/iu")
+                .createdAt(now)
+                .modifiedAt(now)
+                .member(memberMapper.toDomain(member1))
+                .build();
+
+        ArtistApplication savedArtistApplication = artistApplicationAdapter.save(artistApplication);
+
+        assertThat(savedArtistApplication.getStageName()).isEqualTo(artistApplication.getStageName());
+    }
+
+    @DisplayName("아티스트 신청을 상태별로 조회한다.")
+    @ParameterizedTest(name = "{index}: state = {0}")
+    @MethodSource("providedArtistApplicationState")
+    void findArtistApplicationByState(ArtistApplicationState state) {
+        LocalDateTime now = LocalDateTime.now();
+        artistApplicationAdapter.save(ArtistApplication.builder()
+                .stageName("아이유 (IU)")
+                .description("아이유입니다.")
+                .agency("EDAM 엔터테인먼트")
+                .applicationState(state)
+                .profileImageUrl("https://image.com/iu")
+                .youtubeUrl("https://www.youtube.com/iu")
+                .fanCafeUrl("https://cafe.daum.net/iu")
+                .snsUrl("https://instagram.com/iu")
+                .createdAt(now)
+                .modifiedAt(now)
+                .member(memberMapper.toDomain(member1))
+                .build());
+        artistApplicationAdapter.save(ArtistApplication.builder()
+                .stageName("박효신")
+                .description("박효신입니다.")
+                .agency("허비그하로")
+                .applicationState(state)
+                .profileImageUrl("https://image.com/phs")
+                .youtubeUrl("https://www.youtube.com/phs")
+                .fanCafeUrl("https://cafe.daum.net/phs")
+                .snsUrl("https://instagram.com/phs")
+                .createdAt(now)
+                .modifiedAt(now)
+                .member(memberMapper.toDomain(member2))
+                .build());
+
+        Page<ArtistApplication> applications = artistApplicationAdapter.findByArtistApplicationState(
+                PageRequest.of(0, 10), state);
+
+        assertThat(applications).hasSize(2);
+        assertSoftly(as -> {
+            as.assertThat(applications.getContent().get(0).getApplicationState()).isEqualTo(state);
+            as.assertThat(applications.getContent().get(1).getApplicationState()).isEqualTo(state);
+        });
+    }
+
+
+
+
+    private static Stream<Arguments> providedArtistApplicationState() {
+        return Stream.of(
+                Arguments.of(ArtistApplicationState.APPLY),
+                Arguments.of(ArtistApplicationState.REVIEW),
+                Arguments.of(ArtistApplicationState.RETURN),
+                Arguments.of(ArtistApplicationState.APPROVAL)
+        );
+    }
+
+}
