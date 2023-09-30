d54becfb6fe4ca3bfa04b7859db8d5fab37de5ed
김주현 <skydreamer21@naver.com>
2023-09-30 16:59:38 +0900

    [S09P22A105-536] feature: 라이브 등록시 레디스에 시청자 수 및 스트리머 설정 기능 persistence 파트 구현
    - 시청자 수 저장 시 레디스에 키 값으로 sessionId 저장하도록 persistence 파트 수정

--- server/Blooming/src/main/java/com/fivengers/blooming/live/adapter/out/persistence/repository/LivePersistenceAdapter.java
+++ server/Blooming/src/main/java/com/fivengers/blooming/live/adapter/out/persistence/repository/LivePersistenceAdapter.java
@@ -17,10 +17,13 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.data.redis.core.ZSetOperations.TypedTuple;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,12 +35,13 @@ import org.springframework.util.Assert;
 @Transactional(readOnly = true)
 public class LivePersistenceAdapter implements LivePort {
 
-    private final String REDIS_ACTIVE_LIVE_KEY = "activeLive";
+    private final String REDIS_LIVE_VIEWER_COUNT_KEY = "liveViewerCount";
+    private final String REDIS_LIVE_STREAMER_KEY = "liveStreamer";
 
     private final LiveMapper liveMapper;
     private final LiveQueryRepository liveQueryRepository;
     private final LiveSpringDataRepository liveSpringDataRepository;
-    private final RedisTemplate<String, Long> redisTemplate;
+    private final RedisTemplate<String, String> redisTemplate;
 
     @Override
     public Page<Live> findByKeyword(String keyword, Pageable pageable) {
@@ -79,6 +83,27 @@ public class LivePersistenceAdapter implements LivePort {
     }
 
     @Override
+    public void saveActiveLiveInfo(String sessionId, String artistStageName) {
+        System.out.printf("liveId : %s, artistStageName : %s\n", sessionId, artistStageName);
+
+        redisTemplate.execute(new SessionCallback<>() {
+            @Override
+            public Object execute(RedisOperations operations) throws DataAccessException {
+                operations.multi();
+                // Redis에 라이브 생성자 정보 등록
+                operations.opsForHash().put(REDIS_LIVE_STREAMER_KEY, sessionId, artistStageName);
+
+                // Redis에 시청자수 Set 추가 -> 스트리머가 들어갔을 때 0이 되도록 초기값 -1로 설정
+                operations.opsForZSet().add(REDIS_LIVE_VIEWER_COUNT_KEY, sessionId, -1.0);
+
+                return operations.exec();
+            }
+        });
+
+
+    }
+
+    @Override
     public int findLiveCountByWeek(Long artistId, LocalDate endOfWeek) {
         return liveSpringDataRepository.countLiveJpaEntitiesByArtistJpaEntity_IdAndCreatedAtBetween(
                 artistId,
@@ -99,28 +124,31 @@ public class LivePersistenceAdapter implements LivePort {
 
     @Override
     public List<Live> findTopLivesByNumberOfViewers(int numberOfLives) {
-        Set<TypedTuple<Long>> topActiveLiveTuples = redisTemplate.opsForZSet()
-                .rangeByScoreWithScores(REDIS_ACTIVE_LIVE_KEY, 0, 1000, 0, numberOfLives);
+        Set<TypedTuple<String>> topActiveLiveTuples = redisTemplate.opsForZSet()
+                .rangeByScoreWithScores(REDIS_LIVE_VIEWER_COUNT_KEY, 0, 1000, 0, numberOfLives);
         Assert.notNull(topActiveLiveTuples, "시청자 수 정보가 null 일 수 없습니다.");
 
-        Map<Long, Integer> topActiveLivesViewerInfo = convertTupleToMap(topActiveLiveTuples);
+        Map<String, Integer> topActiveLivesViewerInfo = convertTupleToMap(topActiveLiveTuples);
         List<Live> topLives = new ArrayList<>(liveSpringDataRepository.findLiveJpaEntityByIdIsIn(
-                topActiveLivesViewerInfo.keySet()
+                topActiveLivesViewerInfo.keySet().stream()
+                        .map(SessionId::new)
+                        .map(SessionId::getLiveId)
+                        .collect(Collectors.toSet())
         ).stream().map(liveMapper::toDomain).toList());
         topLives.forEach(live -> live.setNumberOfViewers(topActiveLivesViewerInfo.get(live.getId())));
         topLives.sort((live1, live2) -> live2.getNumberOfViewers() - live1.getNumberOfViewers());
         return Collections.unmodifiableList(topLives);
     }
 
-    private Map<Long, Integer> convertTupleToMap(Set<TypedTuple<Long>> typedTuples) {
+    private Map<String, Integer> convertTupleToMap(Set<TypedTuple<String>> typedTuples) {
         return typedTuples.stream().collect(Collectors.toMap(
-                tuple -> Long.valueOf(String.valueOf(tuple.getValue())),
+                TypedTuple::getValue,
                 tuple -> tuple.getScore().intValue()
         ));
     }
 
     @Override
-    public void updateParticipantCount(Long liveId, int difference) {
-        redisTemplate.opsForZSet().incrementScore(REDIS_ACTIVE_LIVE_KEY, liveId, difference);
+    public void updateParticipantCount(String sessionId, int difference) {
+        redisTemplate.opsForZSet().incrementScore(REDIS_LIVE_VIEWER_COUNT_KEY, sessionId, difference);
     }
 }
