67f24a9332a029fc51fae4e96046adee8b80abbd
rt3310 <btac3310@gmail.com>
2023-09-30 16:57:49 +0900

    [S09P22A105-529] feature: 아티스트 신청 변경, 자신 신청 조회 기능 web 로직 구현

--- server/Blooming/src/main/java/com/fivengers/blooming/artist/adapter/in/web/ArtistApplicationAdminController.java
+++ server/Blooming/src/main/java/com/fivengers/blooming/artist/adapter/in/web/ArtistApplicationAdminController.java
@@ -1,7 +1,9 @@
 package com.fivengers.blooming.artist.adapter.in.web;
 
+import com.fivengers.blooming.artist.adapter.in.web.dto.ArtistApplicationDetailsResponse;
 import com.fivengers.blooming.artist.adapter.in.web.dto.ArtistApplicationListResponse;
 import com.fivengers.blooming.artist.application.port.in.ArtistApplicationUseCase;
+import com.fivengers.blooming.artist.application.port.in.dto.ArtistApplicationStateModifyRequest;
 import com.fivengers.blooming.artist.domain.ArtistApplication;
 import com.fivengers.blooming.artist.domain.ArtistApplicationState;
 import com.fivengers.blooming.global.response.ApiResponse;
@@ -11,7 +13,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,5 +43,16 @@ public class ArtistApplicationAdminController {
 
         return ApiResponse.ok(
                 new PageImpl<>(responses, pageable, artistApplications.getTotalElements()));
+    }
+
+    @PutMapping("{applicationId}/states")
+    public ApiResponse<ArtistApplicationDetailsResponse> artistApplicationStateModify(
+            @PathVariable Long applicationId,
+            @RequestBody @Validated ArtistApplicationStateModifyRequest request) {
+        ArtistApplication artistApplication =
+                artistApplicationUseCase.modifyStateById(applicationId, request);
+
+        return ApiResponse.ok(ArtistApplicationDetailsResponse.from(artistApplication,
+                MemberResponse.from(artistApplication.getMember())));
     }
 }
--- server/Blooming/src/main/java/com/fivengers/blooming/artist/adapter/in/web/ArtistApplicationController.java
+++ server/Blooming/src/main/java/com/fivengers/blooming/artist/adapter/in/web/ArtistApplicationController.java
@@ -1,27 +1,19 @@
 package com.fivengers.blooming.artist.adapter.in.web;
 
 import com.fivengers.blooming.artist.adapter.in.web.dto.ArtistApplicationDetailsResponse;
-import com.fivengers.blooming.artist.adapter.in.web.dto.ArtistApplicationListResponse;
 import com.fivengers.blooming.artist.application.port.in.ArtistApplicationUseCase;
 import com.fivengers.blooming.artist.application.port.in.dto.ArtistApplyRequest;
 import com.fivengers.blooming.artist.domain.ArtistApplication;
-import com.fivengers.blooming.artist.domain.ArtistApplicationState;
 import com.fivengers.blooming.config.security.oauth2.LoginUser;
 import com.fivengers.blooming.global.response.ApiResponse;
 import com.fivengers.blooming.member.adapter.in.web.dto.MemberResponse;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -37,6 +29,16 @@ public class ArtistApplicationController {
             @AuthenticationPrincipal LoginUser loginUser) {
         ArtistApplication artistApplication =
                 artistApplicationUseCase.add(request, loginUser.getMemberId());
+
+        return ApiResponse.ok(ArtistApplicationDetailsResponse.from(artistApplication,
+                MemberResponse.from(artistApplication.getMember())));
+    }
+
+    @GetMapping("/me")
+    public ApiResponse<ArtistApplicationDetailsResponse> artistApplicationMyDetails(
+            @AuthenticationPrincipal LoginUser loginUser) {
+        ArtistApplication artistApplication =
+                artistApplicationUseCase.searchById(loginUser.getMemberId());
 
         return ApiResponse.ok(ArtistApplicationDetailsResponse.from(artistApplication,
                 MemberResponse.from(artistApplication.getMember())));
