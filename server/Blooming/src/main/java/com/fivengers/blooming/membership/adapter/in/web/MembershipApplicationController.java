f0531c62a7e92197054913cdf792b1264fd91e4b
rt3310 <btac3310@gmail.com>
2023-10-01 14:16:49 +0900

    [S09P22A105-538] feature: 멤버십 신청 등록, 수정, 조회 기능 web 로직 구현

---
+++ server/Blooming/src/main/java/com/fivengers/blooming/membership/adapter/in/web/MembershipApplicationController.java
@@ -0,0 +1,46 @@
+package com.fivengers.blooming.membership.adapter.in.web;
+
+import com.fivengers.blooming.config.security.oauth2.LoginUser;
+import com.fivengers.blooming.global.response.ApiResponse;
+import com.fivengers.blooming.membership.adapter.in.web.dto.MembershipApplicationDetailsResponse;
+import com.fivengers.blooming.membership.application.port.in.MembershipApplicationUseCase;
+import com.fivengers.blooming.membership.application.port.in.dto.MembershipApplyRequest;
+import com.fivengers.blooming.membership.domain.MembershipApplication;
+import com.fivengers.blooming.project.adapter.in.web.dto.ArtistResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/membership-applications")
+@RequiredArgsConstructor
+public class MembershipApplicationController {
+
+    private final MembershipApplicationUseCase membershipApplicationUseCase;
+
+    @PostMapping
+    public ApiResponse<MembershipApplicationDetailsResponse> membershipApplicationCreate(
+            @RequestBody @Validated MembershipApplyRequest request,
+            @AuthenticationPrincipal LoginUser loginUser) {
+        MembershipApplication membershipApplication = membershipApplicationUseCase.add(request,
+                loginUser.getMemberId());
+
+        return ApiResponse.ok(MembershipApplicationDetailsResponse.from(membershipApplication,
+                ArtistResponse.from(membershipApplication.getArtist())));
+    }
+
+    @GetMapping("/me")
+    public ApiResponse<MembershipApplicationDetailsResponse> membershipApplicationMyDetails(
+            @AuthenticationPrincipal LoginUser loginUser) {
+        MembershipApplication membershipApplication = membershipApplicationUseCase.searchByMemberId(
+                loginUser.getMemberId());
+
+        return ApiResponse.ok(MembershipApplicationDetailsResponse.from(membershipApplication,
+                ArtistResponse.from(membershipApplication.getArtist())));
+    }
+}
