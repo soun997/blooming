5b603c290ca4dc2a91657d37a696f04af4f2252b
김주현 <skydreamer21@naver.com>
2023-10-01 16:15:41 +0900

    [S09P22A105-488] test: Live 종료 end to end 테스트 구현

--- server/Blooming/src/test/java/com/fivengers/blooming/live/LiveRestTest.java
+++ server/Blooming/src/test/java/com/fivengers/blooming/live/LiveRestTest.java
@@ -13,13 +13,19 @@ import com.fivengers.blooming.member.domain.AuthProvider;
 import com.fivengers.blooming.support.RestEndToEndTest;
 import io.restassured.RestAssured;
 import java.time.LocalDateTime;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
 
 public class LiveRestTest extends RestEndToEndTest {
+
+    private final String REDIS_LIVE_VIEWER_COUNT_KEY = "liveViewerCount";
+    private final String REDIS_LIVE_STREAMER_KEY = "liveStreamer";
+    private final String SESSION_PREFIX = "bloomingTest";
 
     @Autowired
     LiveSpringDataRepository liveSpringDataRepository;
@@ -27,16 +33,26 @@ public class LiveRestTest extends RestEndToEndTest {
     ArtistSpringDataRepository artistSpringDataRepository;
     @Autowired
     MemberSpringDataRepository memberSpringDataRepository;
+    @Autowired
+    RedisTemplate<String, String> redisTemplate;
 
     LiveJpaEntity activeLive;
     LiveJpaEntity endedLive;
     ArtistJpaEntity artist;
+    MemberJpaEntity artistMember;
     MemberJpaEntity member;
 
     @BeforeEach
     void initObjects() {
         LocalDateTime now = LocalDateTime.now();
         member = memberSpringDataRepository.save(MemberJpaEntity.builder()
+                .oauth(new Oauth(AuthProvider.KAKAO, "1234567"))
+                .name("Aron")
+                .nickname("Aron")
+                .account("55551234")
+                .deleted(false)
+                .build());
+        artistMember = memberSpringDataRepository.save(MemberJpaEntity.builder()
                 .oauth(new Oauth(AuthProvider.KAKAO, "1234567"))
                 .name("이지은")
                 .nickname("아이유")
@@ -51,7 +67,7 @@ public class LiveRestTest extends RestEndToEndTest {
                 .youtubeUrl("https://youtube.com/iu")
                 .fanCafeUrl("https://cafe.daum.net/ui")
                 .snsUrl("https://instagram.com/ui")
-                .memberJpaEntity(member)
+                .memberJpaEntity(artistMember)
                 .deleted(false)
                 .build());
         activeLive = liveSpringDataRepository.save(LiveJpaEntity.builder()
@@ -77,5 +93,52 @@ public class LiveRestTest extends RestEndToEndTest {
                 .body("results.activeLiveId", response -> equalTo(1));
 
     }
+
+    @Test
+    @DisplayName("아티스트가 자신의 진행 중인 라이브를 종료한다.")
+    void 아티스트가_자신의_진행_중인_라이브를_종료한다() {
+        String sessionId = SESSION_PREFIX + activeLive.getId();
+        redisTemplate.opsForHash().put(REDIS_LIVE_STREAMER_KEY, sessionId,
+                artist.getStageName());
+        redisTemplate.opsForZSet()
+                .add(REDIS_LIVE_VIEWER_COUNT_KEY, sessionId, 5.0);
+
+        RestAssured.given()
+                .header(AUTHORIZATION, getAccessToken(artistMember))
+                .pathParam("liveId", activeLive.getId())
+                .when().put("/api/v1/lives/{liveId}/close")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("results.id", response -> equalTo(1));
+
+        SoftAssertions.assertSoftly(as -> {
+            as.assertThat(redisTemplate.opsForHash().hasKey(REDIS_LIVE_STREAMER_KEY, sessionId)).isFalse();
+            as.assertThat(redisTemplate.opsForZSet().score(REDIS_LIVE_VIEWER_COUNT_KEY, sessionId)).isNull();
+        });
+
+        redisTemplate.opsForHash().delete(REDIS_LIVE_STREAMER_KEY, sessionId);
+        redisTemplate.opsForZSet().remove(REDIS_LIVE_VIEWER_COUNT_KEY, sessionId);
+    }
+    @Test
+    @DisplayName("라이브를 종료할 권한이 없는 사용자가 라이브 종료 요청 시 에러가 발생한다.")
+    void 라이브를_종료할_권한이_없는_사용자가_라이브_종료_요청_시_에러가_발생한다() {
+        String sessionId = SESSION_PREFIX + activeLive.getId();
+        redisTemplate.opsForHash().put(REDIS_LIVE_STREAMER_KEY, sessionId,
+                artist.getStageName());
+        redisTemplate.opsForZSet()
+                .add(REDIS_LIVE_VIEWER_COUNT_KEY, sessionId, 5.0);
+
+        RestAssured.given()
+                .header(AUTHORIZATION, getAccessToken(member))
+                .pathParam("liveId", activeLive.getId())
+                .when().put("/api/v1/lives/{liveId}/close")
+                .then()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .body("results.errorCode", response -> equalTo("ERR_LIVE_005"));
+
+        redisTemplate.opsForHash().delete(REDIS_LIVE_STREAMER_KEY, sessionId);
+        redisTemplate.opsForZSet().remove(REDIS_LIVE_VIEWER_COUNT_KEY, sessionId);
+    }
+
 
 }
