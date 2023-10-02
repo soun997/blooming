b99b81375937db1fbccfe640c3e528868b7fe023
김주현 <skydreamer21@naver.com>
2023-10-02 21:39:43 +0900

    [S09P22A105-486] feature: SocketAuthHandler 구현
    - jwt 토큰 검사
    - AuthSocketUser를 유저 정보로 설정

--- server/Blooming/src/main/java/com/fivengers/blooming/config/security/jwt/JwtValidator.java
+++ server/Blooming/src/main/java/com/fivengers/blooming/config/security/jwt/JwtValidator.java
@@ -2,9 +2,15 @@ package com.fivengers.blooming.config.security.jwt;
 
 import com.fivengers.blooming.config.security.oauth2.LoginUser;
 import com.fivengers.blooming.config.security.oauth2.mapper.LoginUserMapper;
+import com.fivengers.blooming.global.exception.SocketExceptionCode;
+import com.fivengers.blooming.global.exception.stomp.SocketAuthorizationException;
 import com.fivengers.blooming.member.application.port.MemberService;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import java.security.Key;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -26,14 +32,34 @@ public class JwtValidator {
 
     public LoginUser findLoginUserByToken(String token) {
         return loginUserMapper.toLoginUser(
-                memberService.findById(Long.parseLong(getTokenClaims(token).get("id", String.class))));
+                memberService.findById(
+                        Long.parseLong(getTokenClaims(token).get("id", String.class))));
     }
 
-    private Claims getTokenClaims(String token) {
+    public Claims getTokenClaims(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(key)
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
+    }
+
+    public void validateAccessToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+        } catch (MalformedJwtException e) {
+            throw new SocketAuthorizationException(SocketExceptionCode.JWT_MALFORMED);
+        } catch (ExpiredJwtException e) {
+            throw new SocketAuthorizationException(SocketExceptionCode.JWT_EXPIRED);
+        } catch (UnsupportedJwtException e) {
+            throw new SocketAuthorizationException(SocketExceptionCode.JWT_UNSUPPORTED);
+        } catch (SignatureException e) {
+            throw new SocketAuthorizationException(SocketExceptionCode.JWT_INVALID_SIGNATURE);
+        } catch (IllegalArgumentException e) {
+            throw new SocketAuthorizationException(SocketExceptionCode.JWT_PARSING_FAILED, e.getMessage());
+        }
     }
 }
