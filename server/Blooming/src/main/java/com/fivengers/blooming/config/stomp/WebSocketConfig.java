420c6e7b03ffd6c8e6c8dcae92a2ce131c267d73
김주현 <skydreamer21@naver.com>
2023-10-02 21:42:20 +0900

    [S09P22A105-486] feature: AuthSocketHandler, StompExceptionHandler 설정 추가

--- server/Blooming/src/main/java/com/fivengers/blooming/config/stomp/WebSocketConfig.java
+++ server/Blooming/src/main/java/com/fivengers/blooming/config/stomp/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.fivengers.blooming.config.stomp;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -12,9 +13,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    private final StompExceptionHandler stompExceptionHandler;
+    private final AuthSocketHandler authSocketHandler;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/blooming")
+        registry.setErrorHandler(stompExceptionHandler)
+                .addEndpoint("/ws/blooming")
                 .setAllowedOriginPatterns("http://localhost:3000", "https://j9a105.p.ssafy.io")
                 .withSockJS();
     }
@@ -24,5 +29,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.enableSimpleBroker("/topic/", "/queue/");
         registry.setApplicationDestinationPrefixes("/ws/pub");
         registry.setUserDestinationPrefix("/user");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(authSocketHandler);
     }
 }
