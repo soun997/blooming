6edf844cd77c669011c949f5d447f3ad074fa612
rt3310 <btac3310@gmail.com>
2023-10-02 18:54:52 +0900

    [S09P22A105-INTEGRITY] test: test 코드 style 정리

--- server/Blooming/src/test/java/com/fivengers/blooming/support/RestEndToEndTest.java
+++ server/Blooming/src/test/java/com/fivengers/blooming/support/RestEndToEndTest.java
@@ -6,7 +6,6 @@ import com.fivengers.blooming.config.security.jwt.JwtProvider;
 import com.fivengers.blooming.config.security.oauth2.OAuth2Request;
 import com.fivengers.blooming.config.security.oauth2.mapper.LoginUserMapper;
 import com.fivengers.blooming.member.adapter.out.persistence.entity.MemberJpaEntity;
-import com.fivengers.blooming.member.adapter.out.persistence.entity.Oauth;
 import com.fivengers.blooming.member.adapter.out.persistence.mapper.MemberMapper;
 import com.fivengers.blooming.member.application.port.MemberService;
 import com.fivengers.blooming.member.domain.AuthProvider;
@@ -15,23 +14,17 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 public class RestEndToEndTest {
 
-    @Autowired
-    private ObjectMapper objectMapper;
-    @Autowired
-    private JwtProvider jwtProvider;
-    @Autowired
-    private MemberService memberService;
-    @Autowired
-    private MemberMapper memberMapper;
-    @Autowired
-    private LoginUserMapper loginUserMapper;
+    @Autowired private ObjectMapper objectMapper;
+    @Autowired private JwtProvider jwtProvider;
+    @Autowired private MemberService memberService;
+    @Autowired private MemberMapper memberMapper;
+    @Autowired private LoginUserMapper loginUserMapper;
 
-    protected Member member;
+    protected Member authMember;
 
     public static final String AUTHORIZATION = "Authorization";
 
@@ -40,13 +33,13 @@ public class RestEndToEndTest {
     }
 
     protected String getAccessToken() {
-        member = memberService.saveIfNotExists(
+        authMember = memberService.saveIfNotExists(
                 new OAuth2Request(UUID.randomUUID().toString(),
                         AuthProvider.KAKAO,
                         "mock",
                         "mock"));
         return jwtProvider.createJwtToken(loginUserMapper.toLoginUser(
-                        member))
+                        authMember))
                 .getAccessToken();
     }
 
