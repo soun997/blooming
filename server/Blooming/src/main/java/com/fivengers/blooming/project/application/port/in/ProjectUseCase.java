31bf0595dc85dd2482239e7cff624659d6597b56
soyun <soun997@gmail.com>
2023-10-03 22:39:06 +0900

    [S09P22A105-552] feature: 아티스트 ID 기반 최근 펀딩 프로젝트 목록 조회 비즈니스 로직 구현

---
+++ server/Blooming/src/main/java/com/fivengers/blooming/project/application/port/in/ProjectUseCase.java
@@ -0,0 +1,9 @@
+package com.fivengers.blooming.project.application.port.in;
+
+import com.fivengers.blooming.project.domain.Project;
+import java.util.List;
+
+public interface ProjectUseCase {
+
+    List<Project> searchProjectsById(Long artistId);
+}
