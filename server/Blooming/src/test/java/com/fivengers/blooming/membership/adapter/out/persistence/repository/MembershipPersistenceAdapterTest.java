ac9ba19e6891658c847977acf357ad3b362672ea
rt3310 <btac3310@gmail.com>
2023-10-03 22:49:54 +0900

    [S09P22A105-546] fix: adapter test에 transactional 처리 추가

--- server/Blooming/src/test/java/com/fivengers/blooming/membership/adapter/out/persistence/repository/MembershipPersistenceAdapterTest.java
+++ server/Blooming/src/test/java/com/fivengers/blooming/membership/adapter/out/persistence/repository/MembershipPersistenceAdapterTest.java
@@ -22,8 +22,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 class MembershipPersistenceAdapterTest {
 
     @Autowired MembershipSpringDataRepository membershipSpringDataRepository;
