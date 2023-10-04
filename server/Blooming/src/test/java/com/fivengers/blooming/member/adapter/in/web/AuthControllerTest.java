6b7ada6e394256a64326ba48d843f0937261fb59
rt3310 <btac3310@gmail.com>
2023-10-04 14:08:13 +0900

    [S09P22A105-548] test: AuthController 테스트 코드 작성

---
+++ server/Blooming/src/test/java/com/fivengers/blooming/member/adapter/in/web/AuthControllerTest.java
@@ -0,0 +1,79 @@
+package com.fivengers.blooming.member.adapter.in.web;
+
+import static com.fivengers.blooming.support.docs.ApiDocumentUtils.getDocumentRequest;
+import static com.fivengers.blooming.support.docs.ApiDocumentUtils.getDocumentResponse;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+import com.fivengers.blooming.config.security.jwt.JwtProvider;
+import com.fivengers.blooming.config.security.jwt.JwtToken;
+import com.fivengers.blooming.config.security.jwt.JwtValidator;
+import com.fivengers.blooming.config.security.oauth2.LoginUser;
+import com.fivengers.blooming.member.application.port.in.dto.AuthRequest;
+import com.fivengers.blooming.member.domain.AuthProvider;
+import com.fivengers.blooming.member.domain.Member;
+import com.fivengers.blooming.member.domain.MemberRole;
+import com.fivengers.blooming.support.docs.RestDocsTest;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+
+@WebMvcTest(AuthController.class)
+class AuthControllerTest extends RestDocsTest {
+
+    @MockBean
+    JwtValidator jwtValidator;
+    @MockBean
+    JwtProvider jwtProvider;
+
+    @Test
+    @DisplayName("authToken으로 Jwt 정보를 가져온다.")
+    void getJwtByAuthToken() throws Exception {
+        AuthRequest request = new AuthRequest("authToken");
+        LocalDateTime now = LocalDateTime.now();
+        Member member = Member.builder()
+                .id(1L)
+                .oauthProvider(AuthProvider.KAKAO)
+                .oauthAccount("12434512")
+                .name("이지은")
+                .nickname("아이유")
+                .account("account")
+                .createdAt(now)
+                .modifiedAt(now)
+                .role(List.of(MemberRole.ROLE_USER))
+                .build();
+        Map<String, Object> attributes = new HashMap<>();
+        attributes.put("id", member.getId());
+        given(jwtValidator.findLoginUserByToken(any(String.class)))
+                .willReturn(new LoginUser(member, attributes, member.getAuthority()));
+        given(jwtProvider.createJwtToken(any(LoginUser.class)))
+                .willReturn(new JwtToken("accessToken", "refreshToken", "Bearer"));
+
+        ResultActions perform = mockMvc.perform(post("/api/v1/auth")
+                .content(toJson(request))
+                .contentType(MediaType.APPLICATION_JSON));
+
+        perform.andExpect(status().isOk());
+
+        perform.andDo(print())
+                .andDo(document("auth",
+                        getDocumentRequest(),
+                        getDocumentResponse()));
+    }
+}
