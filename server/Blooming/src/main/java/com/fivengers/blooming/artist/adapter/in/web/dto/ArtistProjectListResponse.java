add1124fa7969961b4503e14a02035b2b49a1591
soyun <soun997@gmail.com>
2023-10-04 14:38:45 +0900

    [S09P22A105-556] feature: ArtistProjectListResponse에 type 컬럼 추가
    콘서트/활동 펀딩을 구분하기 위함

--- server/Blooming/src/main/java/com/fivengers/blooming/artist/adapter/in/web/dto/ArtistProjectListResponse.java
+++ server/Blooming/src/main/java/com/fivengers/blooming/artist/adapter/in/web/dto/ArtistProjectListResponse.java
@@ -1,12 +1,15 @@
 package com.fivengers.blooming.artist.adapter.in.web.dto;
 
 import com.fivengers.blooming.project.domain.Project;
+import com.fivengers.blooming.project.domain.ProjectType;
 
 public record ArtistProjectListResponse(Long id,
-                                        String thumbnail) {
+                                        String thumbnail,
+                                        ProjectType type) {
 
     public static ArtistProjectListResponse from(Project project) {
 
-        return new ArtistProjectListResponse(project.getId(), project.getProfileImg());
+        return new ArtistProjectListResponse(
+                project.getId(), project.getProfileImg(), project.getDtype());
     }
 }
