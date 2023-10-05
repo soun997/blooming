f8e4a9a1e4e0b4230e7e99b054c13fd61ef9b2fc
김주현 <skydreamer21@naver.com>
2023-10-05 13:15:57 +0900

    [S09P22A105-496] fix: WebSocketConfig url 환경변수로 수정

--- server/Blooming/src/main/java/com/fivengers/blooming/config/stomp/WebSocketConfig.java
+++ server/Blooming/src/main/java/com/fivengers/blooming/config/stomp/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.fivengers.blooming.config.stomp;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -16,11 +17,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     private final StompExceptionHandler stompExceptionHandler;
     private final AuthSocketHandler authSocketHandler;
 
+    @Value("${client.url}")
+    private String clientUrl;
+    @Value("${DOMAIN_URL}")
+    private String domainUrl;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.setErrorHandler(stompExceptionHandler)
                 .addEndpoint("/ws/blooming")
-                .setAllowedOriginPatterns("http://localhost:5173", "https://j9a105.p.ssafy.io")
+                .setAllowedOriginPatterns(clientUrl, domainUrl)
                 .withSockJS();
     }
 
