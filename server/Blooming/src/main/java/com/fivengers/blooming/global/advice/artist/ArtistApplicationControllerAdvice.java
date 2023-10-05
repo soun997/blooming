642e97ebf134d56e1912093f736086eaa0aaa748
rt3310 <btac3310@gmail.com>
2023-10-05 15:29:56 +0900

    [S09P22A105-572] feature: exception handler 정의

--- server/Blooming/src/main/java/com/fivengers/blooming/global/advice/artist/ArtistApplicationControllerAdvice.java
+++ server/Blooming/src/main/java/com/fivengers/blooming/global/advice/artist/ArtistApplicationControllerAdvice.java
@@ -2,6 +2,8 @@ package com.fivengers.blooming.global.advice.artist;
 
 import com.fivengers.blooming.global.advice.AdviceLoggingUtils;
 import com.fivengers.blooming.global.exception.artist.ArtistApplicationNotFoundException;
+import com.fivengers.blooming.global.exception.artist.ArtistNotFoundException;
+import com.fivengers.blooming.global.exception.artist.InvalidArtistApplicationStateException;
 import com.fivengers.blooming.global.response.ApiResponse;
 import com.fivengers.blooming.global.response.ErrorResponse;
 import org.springframework.http.HttpStatus;
@@ -10,13 +12,21 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class ArtistApplicationAdvice {
+public class ArtistApplicationControllerAdvice {
 
     @ExceptionHandler(ArtistApplicationNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ApiResponse<ErrorResponse> artistApplicationNotFoundException(
+    public ApiResponse<ErrorResponse> artistApplicationNotFound(
             ArtistApplicationNotFoundException e) {
         AdviceLoggingUtils.exceptionLog(e);
         return ApiResponse.notFound(ErrorResponse.from(e.getExceptionCode()));
+    }
+
+    @ExceptionHandler(InvalidArtistApplicationStateException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiResponse<ErrorResponse> invalidArtistApplicationState(
+            InvalidArtistApplicationStateException e) {
+        AdviceLoggingUtils.exceptionLog(e);
+        return ApiResponse.badRequest(ErrorResponse.from(e.getExceptionCode()));
     }
 }
