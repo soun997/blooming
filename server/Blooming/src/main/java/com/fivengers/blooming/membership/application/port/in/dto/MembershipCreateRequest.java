c831362edf3ff832834489dd0d5bb9257320b139
soyun <soun997@gmail.com>
2023-10-05 17:53:44 +0900

    [S09P22A105-575] refactor: Membership Create Request DTO 누락된 컬럼 추가

--- server/Blooming/src/main/java/com/fivengers/blooming/membership/application/port/in/dto/MembershipCreateRequest.java
+++ server/Blooming/src/main/java/com/fivengers/blooming/membership/application/port/in/dto/MembershipCreateRequest.java
@@ -3,30 +3,38 @@ package com.fivengers.blooming.membership.application.port.in.dto;
 import com.fivengers.blooming.artist.domain.Artist;
 import com.fivengers.blooming.membership.domain.Membership;
 import com.fivengers.blooming.membership.domain.NftSale;
-import com.fivengers.blooming.nft.domain.Nft;
 import java.time.LocalDateTime;
 
 public record MembershipCreateRequest(String title,
+                                      String symbol,
                                       String description,
                                       Integer season,
                                       LocalDateTime seasonStart,
                                       LocalDateTime seasonEnd,
                                       LocalDateTime purchaseStart,
                                       LocalDateTime purchaseEnd,
+                                      Integer saleCount,
+                                      Long salePrice,
                                       String thumbnailUrl,
+                                      String baseUri,
+                                      String contractAddress,
                                       Long artistId) {
 
     public Membership toDomain(Artist artist) {
         return Membership.builder()
                 .title(title)
+                .symbol(symbol)
                 .description(description)
                 .season(season)
                 .seasonStart(seasonStart)
                 .seasonEnd(seasonEnd)
                 .purchaseStart(purchaseStart)
                 .purchaseEnd(purchaseEnd)
-                .saleCount(0)
+                .saleCount(saleCount)
+                .salePrice(salePrice)
                 .thumbnailUrl(thumbnailUrl)
+                .baseUri(baseUri)
+                .contractAddress(contractAddress)
                 .artist(artist)
                 .nftSale(NftSale.builder()
                         .totalNftCount(0)
